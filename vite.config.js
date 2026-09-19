ce0f1b75437a05240f3e90115ad24bdd425af874
Anshmangithub <anshman5566@gmail.com>
2026-09-19 20:29:49 +0530

    fix the vercel issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isVercel = Boolean(process.env.VERCEL);
+
 export default defineConfig({
-  base: "/ansh_portfolio/",
+  base: isVercel ? "/" : "/ansh_portfolio/",
   plugins: [react()],
   server: {
     port: 3000, // Change this if needed
